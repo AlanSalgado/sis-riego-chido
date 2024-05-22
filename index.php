a7ffb210d311cb2c4e972c60d1fc6d32bf47e3e7
AlanSalgado <94387803+AlanSalgado@users.noreply.github.com>
2024-05-21 23:18:56 -0600

    Cambio de codigo

--- index.php
+++ index.php
@@ -1,27 +1,19 @@
 <?php
-$servername = "servidor-sis-riego-chido.mysql.database.azure.com"; // Cambia esto
-$username = "chalinosanchez"; // Cambia esto
-$password = "Ch4l1n0S4nch3z"; // Cambia esto
-$dbname = "sistemariego"; // Cambia esto
+    $host = 'servidor-sis-riego-chido.mysql.database.azure.com';
+    $username = 'chalinosanchez';
+    $password = '"Ch4l1n0S4nch3z';
+    $db_name = 'sistemariego';
+    
 
-// Crear la conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+    //Initializes MySQLi
+    $conn = mysqli_init();
 
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
+    // Establish the connection
+    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
 
-$sql = "SELECT * FROM prueba"; // Cambia esto
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Salida de datos de cada fila
-    while($row = $result->fetch_assoc()) {
-        echo "humedad: " . $row["humedad"]. " - regado: " . $row["regado"]. " " . $row["apellido"]. "<br>";
+    //If connection failed, show the error
+    if (mysqli_connect_errno())
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
     }
-} else {
-    echo "0 resultados";
-}
-$conn->close();
 ?>
