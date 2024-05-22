979f1d1d686769d5558505a7d4953b13ea67abfa
AlanSalgado <94387803+AlanSalgado@users.noreply.github.com>
2024-05-22 15:21:16 -0600

    Ya se actualiza automáticamente

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="refresh" content="5">
         <title>Sistema de Riego</title>
         <style>
             body {
